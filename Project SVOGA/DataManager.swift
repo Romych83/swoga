d1fbb4ccabd04ec5155b523ccfbd48c943311ee3
romych83 <topinambu2@gmail.com>
2018-12-20 17:19:23 +0200

    DataManager

--- Project SVOGA/DataManager.swift
+++ Project SVOGA/DataManager.swift
@@ -1,78 +1,41 @@
-//
 //  DataManager.swift
 //  Project SVOGA
 //
 //  Created by ROMYCH on 04.12.18.
 //  Copyright © 2018 Romych. All rights reserved.
-//
-//
-//import UIKit
-//import Foundation
-//import CoreData 
-//
-//
-//// MARK: - Protocol
-//
-//protocol DataManagerProtocol {
-//    func saveGas(_ history: GasStatistics)
-//    func getHistory() -> [GasStatistics]?
-//    func deleteGas()
-//}
-//
-//final class DataManager: DataManagerProtocol {
-//    
-//    // MARK: - Private enums
-//    
-//    private enum DataStrings: String {
-//        case GasDataModel
-//        
-//    }
-//    
-//    // MARK: - Lazy properties
-//    
-//    lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-//    
-//    // MARK: - Open functions
-//    
-//    // gas
-//    func saveGas(_ gas: GasStatistics) {
-//        
-//        let gasModel = GasDataModel(context: context)
-//        gasModel.date = gas.date
-//        gasModel.gas = gas.gas
-//        
-//        do {
-//            try context.save()
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//    }
-//    
-//}
-//
-//func getGas() -> [GasStatistics]? {
-//    let request = NSFetchRequest<HistoryDataModel>(entityName: DataStrings.GasDataModel.rawValue)
-//    var gasData = [GasStatistics]()
-//    do {
-//        let gasHistory = try context.fetch(request)
-//        gasHistory.forEach { (gas) in
-//            guard let gas = Gas(from: gas) else { return }
-//            gasData.append(gas)
-//        }
-//        return gasData
-//    } catch {
-//        print(error.localizedDescription)
-//        return nil
-//    }
-//}
-//
-//func deleteGas() {
-//    let request = NSFetchRequest<NSFetchRequestResult>(entityName: DataStrings.GasDataModel.rawValue)
-//    let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
-//    do {
-//        try context.execute(deleteRequest)
-//    } catch {
-//        print(error.localizedDescription)
-//    }
-//}
-//
+
+
+import UIKit
+import Foundation
+import CoreData
+
+final class DataManager {
+    
+    lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    func getGas() -> GasStatistics {
+        
+        let userFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "GasDataModel")
+        userFetch.fetchLimit = 1
+        userFetch.sortDescriptors = [NSSortDescriptor.init(key: "gas", ascending: true)]
+        
+        let request = try! context.fetch(userFetch)
+        let gas: GasStatistics = request.first as! GasStatistics
+        return gas
+    }
+    
+    func saveGas(data: String) {
+        
+        let gasEntity = NSEntityDescription.entity(forEntityName: "GasDataModel", in: context)!
+        let gasRead = NSManagedObject(entity: gasEntity, insertInto: context)
+        
+        gasRead.setValue(Float(data), forKeyPath: "gas")
+        
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+}
